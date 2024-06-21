ae7ae984a2cdec252bf35446a55896c23f44149b
Roland-N <nicolairoland@icloud.com>
2024-06-21 16:55:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,21 +12,20 @@ with st.sidebar:
 
 st.title("💬 Chatbot")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+model = genai.GenerativeModel("gemini-1.5-flash") 
+chat = model.start_chat()
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+def LLM_Response(question):
+    response = chat.send_message(question,stream=True)
+    return response
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+st.title("Chat Application using Gemini Pro")
 
-    client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+user_quest = st.text_input("Ask a question:")
+btn = st.button("Ask")
+
+if btn and user_quest:
+    result = LLM_Response(user_quest)
+    st.subheader("Response : ")
+    for word in result:
+        st.text(word.text)
