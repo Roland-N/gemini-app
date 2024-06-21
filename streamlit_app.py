e778936f757112b5e0a8d896c608a22b9b61d986
Roland-N <nicolairoland@icloud.com>
2024-06-21 17:11:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,11 @@ def LLM_Response(question):
     response = chat.send_message(question,stream=True)
     return response
 
-
+instruction = "Make a joke about topic: "
 user_quest = st.text_input("Ask a question:")
 btn = st.button("Ask")
 if btn and user_quest:
-    result = LLM_Response(user_quest)
+    result = LLM_Response(instruction + user_quest)
     st.subheader("Response : ")
     for word in result:
         st.text(word.text)
