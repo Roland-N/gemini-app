1eabedb0e40764672a4660adbb87012e70c3072c
Roland-N <nicolairoland@icloud.com>
2024-06-21 16:30:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,26 @@ import streamlit as st
 checkout [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 """
+
+import google.generativeai as genai
+
+
+genai.configure(api_key = "AIzaSyAg3REDWkGV7hyVJpOlZjtj73CjQi37TcQ")
+
+model = genai.GenerativeModel("gemini-1.5-flash") 
+chat = model.start_chat()
+
+def LLM_Response(question):
+    response = chat.send_message(question,stream=True)
+    return response
+
+st.title("Chat Application using Gemini Pro")
+
+user_quest = st.text_input("Ask a question:")
+btn = st.button("Ask")
+
+if btn and user_quest:
+    result = LLM_Response(user_quest)
+    st.subheader("Response : ")
+    for word in result:
+        st.text(word.text)
