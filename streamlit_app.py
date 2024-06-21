bbb7eab1e3c8810f48d751c9a470a72ba1568d9b
Roland-N <nicolairoland@icloud.com>
2024-06-21 16:52:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ import google.generativeai as genai
 import streamlit as st
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    gemini_api_key = st.text_input("Gemini API Key", key="chatbot_api_key", type="password")
+    genai.configure(api_key= gemini_api_key)
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
